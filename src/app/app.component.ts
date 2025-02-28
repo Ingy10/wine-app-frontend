6fd0d9c2fd704908ad2edd2849c9e2c090c91346
Kyle Ingham <kyleingy@proton.me>
2025-02-28 11:54:02 -0700

    Initialized primeng theme.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, CardModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'wine-app-frontend';
+  title = 'Wine Knowledge App';
 }
